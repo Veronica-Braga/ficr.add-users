129167d9a4e9adeb4e563011361988194ff18f5a
Veronica Braga <veronica.braga@a.ficr.edu.br>
2021-08-25 09:46:09 -0300

    Criação do server

---
+++ server.js
@@ -0,0 +1,18 @@
+// importando o express
+const express = require ('express')
+
+//Criando uma instancia do express
+const app = express()
+
+// Definido a porta que vai escutar a aplicação 
+// Usar a porta definida na variável de ambiente PORT ou usar a 3000
+const port = process.env.PORT || 3003
+
+app.get('/', (req, res) => {
+	return res.send ('API de cadastro de usuários')
+})
+
+app.listen(port)
+
+// para mostrar que o servidor está rodando
+console.log('Servidor funcionando na porta', port)
